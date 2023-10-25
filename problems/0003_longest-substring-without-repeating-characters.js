6011849f18e077f23dd8e56e188b180852adbc62
thomasryu <thomas.sameshima@gmail.com>
2023-10-25 20:43:33 -0300

    Adding problems 2 and 3

---
+++ problems/0003_longest-substring-without-repeating-characters.js
@@ -0,0 +1,52 @@
+/*
+
+Given a string s, find the length of the longest
+substring without repeating characters.
+
+
+Example 1:
+
+Input: s = "abcabcbb"
+Output: 3
+Explanation: The answer is "abc", with the length of 3.
+
+
+Example 2:
+
+Input: s = "bbbbb"
+Output: 1
+Explanation: The answer is "b", with the length of 1.
+Example 3:
+
+
+Input: s = "pwwkew"
+Output: 3
+Explanation: The answer is "wke", with the length of 3.
+Notice that the answer must be a substring, "pwke" is a subsequence and not a substring.
+
+
+Constraints:
+- 0 <= s.length <= 5 * 104
+- s consists of English letters, digits, symbols and spaces.
+
+*/
+
+var lengthOfLongestSubstring = function (s) {
+  const n = s.length
+  const set = new Set()
+
+  let result = 0
+  let head = 0
+
+  // Standard caterpillar to find the optimal substring
+  for (let tail = 0; tail < n; tail++) {
+    while (!set.has(s[head]) && head < n) {
+      set.add(s[head])
+      head++
+    }
+    result = Math.max(result, set.size)
+    set.delete(s[tail])
+  }
+
+  return result
+}
