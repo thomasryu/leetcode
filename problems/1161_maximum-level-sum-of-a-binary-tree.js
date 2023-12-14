97dda0bd46cdddbbdab90b1dc03028b02ecb2cf8
thomasryu <thomas.sameshima@gmail.com>
2023-12-14 17:09:17 -0300

    Finishing "Binary Tree - BFS" section of "LeetCode 75"

---
+++ problems/1161_maximum-level-sum-of-a-binary-tree.js
@@ -0,0 +1,60 @@
+/*
+
+Given the root of a binary tree,
+the level of its root is 1, the level of its children is 2, and so on.
+
+Return the smallest level x such that
+the sum of all the values of nodes at level x is maximal.
+
+Example 1:
+  Input: root = [1,7,0,7,-8,null,null]
+  Output: 2
+  Explanation:
+    Level 1 sum = 1.
+    Level 2 sum = 7 + 0 = 7.
+    Level 3 sum = 7 + -8 = -1.
+    So we return the level with the maximum sum which is level 2.
+
+Example 2:
+  Input: root = [989,null,10250,98693,-89388,null,null,null,-32127]
+  Output: 2
+
+Constraints:
+- The number of nodes in the tree is in the range [1, 104].
+- -105 <= Node.val <= 105
+
+*/
+
+var maxLevelSum = function (root) {
+  let level = [root]
+  let nextLevel = []
+
+  let sum = 0
+  let max = -Infinity
+
+  let current = 1
+  let result = 0
+
+  while (level.length) {
+    const node = level.shift()
+    sum += node.val
+
+    if (node.left) nextLevel.push(node.left)
+    if (node.right) nextLevel.push(node.right)
+
+    if (level.length == 0) {
+      level = nextLevel
+      nextLevel = []
+
+      if (sum > max) {
+        max = sum
+        result = current
+      }
+
+      sum = 0
+      current++
+    }
+  }
+
+  return result
+}
