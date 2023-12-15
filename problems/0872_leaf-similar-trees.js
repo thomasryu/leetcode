113c1a132b857c97341d24d440f1fa27c3203c0f
thomasryu <thomas.sameshima@gmail.com>
2023-12-15 17:13:18 -0300

    Finishing "Binary Tree - DFS" section of "LeetCode 75"

---
+++ problems/0872_leaf-similar-trees.js
@@ -0,0 +1,47 @@
+/*
+
+Consider all the leaves of a binary tree, from left to right order,
+the values of those leaves form a leaf value sequence.
+
+For example, in the given tree above,
+the leaf value sequence is (6, 7, 4, 9, 8).
+
+Two binary trees are considered leaf-similar
+if their leaf value sequence is the same.
+
+Return true if and only if the two given trees
+with head nodes root1 and root2 are leaf-similar.
+
+Example 1:
+  Input: root1 = [3,5,1,6,2,9,8,null,null,7,4],
+         root2 = [3,5,1,6,7,4,2,null,null,null,null,null,null,9,8]
+  Output: true
+
+Example 2:
+  Input: root1 = [1,2,3], root2 = [1,3,2]
+  Output: false
+
+Constraints:
+- The number of nodes in each tree will be in the range [1, 200].
+- Both of the given trees will have values in the range [0, 200].
+
+*/
+
+var leafSimilar = function (root1, root2) {
+  const leaves = []
+
+  const navigate = (node) => {
+    if (!node.left && !node.right) leaves.push(node.val)
+    if (node.left) navigate(node.left)
+    if (node.right) navigate(node.right)
+  }
+
+  const compare = (node) => {
+    if (!node) return true
+    if (!node.left && !node.right) if (leaves.shift() != node.val) return false
+    return compare(node.left) && compare(node.right)
+  }
+
+  navigate(root1)
+  return compare(root2) && leaves.length == 0
+}
