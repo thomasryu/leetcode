5e79515b91c130a0dab3b5d05c93c6fec7a6e18c
thomasryu <thomas.sameshima@gmail.com>
2024-01-17 15:11:30 -0300

    Doing interview challenges

--- problems/0857_minimum-cost-to-hire-k-workers.js
+++ problems/0857_minimum-cost-to-hire-k-workers.js
@@ -1,57 +1,69 @@
+/*
+
+There are n workers. You are given two integer arrays quality and wage where quality[i]
+is the quality of the ith worker and wage[i] is the minimum wage expectation for the ith worker.
+
+We want to hire exactly k workers to form a paid group.
+To hire a group of k workers, we must pay them according to the following rules:
+
+- Every worker in the paid group should be paid in the ratio of their quality compared to other workers in the paid group.
+- Every worker in the paid group must be paid at least their minimum wage expectation.
+
+Given the integer k, return the least amount of money needed to form a paid group satisfying the above conditions.
+Answers within 10-5 of the actual answer will be accepted.
+
+Example 1:
+  Input: quality = [10,20,5], wage = [70,50,30], k = 2
+  Output: 105.00000
+  Explanation: We pay 70 to 0th worker and 35 to 2nd worker.
+
+Example 2:
+  Input: quality = [3,1,10,10,1], wage = [4,8,2,2,7], k = 3
+  Output: 30.66667
+  Explanation: We pay 4 to 0th worker, 13.33333 to 2nd and 3rd workers separately.
+
+Constraints:
+- n == quality.length == wage.length
+- 1 <= k <= n <= 104
+- 1 <= quality[i], wage[i] <= 104
+
+*/
+
 var mincostToHireWorkers = function (quality, wage, k) {
-  // Each worker has a (wage / quality)
-  // we need to minimize total quality * max(wagei / qualityi)
-  const n = quality.length
+  // The salary of the group is defined by the worker with the
+  // highest ratio = wage / quality among them.
 
-  // dp[i][j] will give me [total_quality, max_ratio] which minimizes the amount
-  // of money needed when picking the i-th worker from the 0 to j-th workers
-  const dp = Array(k)
-    .fill()
-    .map(() => Array(n).fill([]))
+  // So to minimize our max wage, we want to minimize the
+  // ratio among the members of the group.
 
-  for (let i = 0; i < n; i++) dp[0][i] = [wage[i] / quality[i], quality[i]]
+  // So we start with the K workers with the smallest ratios,
+  // save the highest ratio and, for each new worker, in increasing
+  // ratio order:
 
-  if (k == 1) return Math.min(...dp[0].map(x => x[0] * x[1]))
+  // 1. We pick the current hired worker with the highest quality
+  //    (because they are the one currently affecting our total wage the most)
+  // 2. Subtract their quality from the current total
+  // 3. Add the new worker's quality to the total
+  // 4. Calculate the new total pay and compare with the current result
 
-  for (let i = 1; i <= k; i++) {
-    for (let j = 0; j < n; j++) {
-      const curr_ratio = wage[j] / quality[j]
-      const curr_quality = quality[j]
+  let result = Infinity
+  const workers = quality.map((q, i) => [wage[i] / q, q]).sort((a, b) => a[0] - b[0])
+  const max_quality = new MaxPriorityQueue({ priority: (e) => e[1] }) // sorted by highest quality
 
-      // For our current choice, we can either choose to keep the previous
-      // [i, j - 1] choice or add the j-th worker to the [i - 1, j - 1] one
+  let total_quality = 0
+  let max_ratio = 0
+  for (let i = 0; i < workers.length; i++) {
+    const [new_ratio, new_quality] = workers[i]
+    max_ratio = new_ratio
+    total_quality += new_quality
 
-      if (j < i) continue // There are less workers than choices
-      if (j == i) {
-        // Our only choice is to add the current worker
-        const [prev_ratio, total] = dp[i - 1][j - 1]
-        dp[i][j] = [Math.max(prev_ratio, curr_ratio), total + curr_quality]
-        continue
-      }
+    // If our hired workers reached limit,
+    // we have to remove one first
+    if (i >= k) total_quality -= max_quality.dequeue().element[1]
+    max_quality.enqueue(workers[i])
 
-      const keep_cost = dp[i][j - 1][0] * dp[i][j - 1][1]
-      let add_cost = Infinity
-      let add_index = -1
-
-      for (let l = i - 1; l < j; l++) {
-        const test_ratio = Math.max(dp[i - 1][l][0], curr_ratio)
-        const test_total = dp[i - 1][l][1] + curr_quality
-        const test_cost = test_ratio * test_total
-
-        if (test_cost < add_cost) {
-          add_index = l
-          add_cost = test_cost
-        }
-      }
-
-      if (keep_cost <= add_cost) dp[i][j] = dp[i][j - ]
-      else
-        dp[i][j] = [
-          Math.max(dp[i - 1][add_index][0], curr_ratio),
-          dp[i - 1][add_index][1] + curr_quality,
-        ]
-    }
+    if (max_quality.size() == k) result = Math.min(result, total_quality * max_ratio)
   }
 
-  return dp[k - 1][n - 1][0] * dp[k - 1][n - 1][1]
+  return result
 }
