4b06c57d889f79e1d5046e43b4f0bdb343ab2905
thomasryu <thomas.sameshima@gmail.com>
2024-02-27 15:58:35 -0300

    Completing daily challenge

--- problems/0100_same-tree.js
+++ problems/0100_same-tree.js
@@ -29,3 +29,10 @@ var isSameTree = function (p, q) {
   if (p?.val !== q?.val) return false
   return isSameTree(p.left, q.left) && isSameTree(p.right, q.right)
 }
+
+// Attempt made at 26/02/2024
+var isSameTree = function (p, q) {
+  if (p == null && q == null) return true
+  if (p?.val != q?.val) return false
+  return isSameTree(p.left, q.left) && isSameTree(p.right, q.right)
+}
